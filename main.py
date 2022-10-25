c45d0837ed2e9427bdcbc8536574ef39e296d015
The Loraxxz <tilmann.lorenz@mail.com>
2022-10-25 17:09:49 +0200

    new pin try

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
+
+pin = 29
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(15,GPIO.OUT)
-GPIO.output(15,False)
+GPIO.setup(pin,GPIO.OUT)
+GPIO.output(pin,False)
 print("set low")
-GPIO.output(15,True)
+GPIO.output(pin,True)
 print("set high")
 time.sleep(10)
-GPIO.output(15,False)
+GPIO.output(pin,False)
 print("set low")
 #me.sleep(rand.randint(0,45))
 GPIO.cleanup()
